ca4bfc80bcb6daf4587576f08684516ca4ce7f0d
vkholic18 <106223106+vkholic18@users.noreply.github.com>
2025-03-17 14:27:12 +0530

    Update and rename m.scala to m.sql

---
+++ m.sql
@@ -0,0 +1,48 @@
+-- Creating temporary table for classification_df
+CREATE TEMP TABLE classification_df AS
+SELECT *
+FROM SP_Classification
+WHERE DATE(outUTC) = CURRENT_DATE
+  AND DATE(inUTC) = CURRENT_DATE;
+
+-- Creating temporary table for exclude_classification_df
+CREATE TEMP TABLE exclude_classification_df AS
+SELECT assetId AS exclude_assetId
+FROM classification_df
+WHERE classificationId = 591;
+
+-- Creating temporary table for deployments_df
+CREATE TEMP TABLE deployments_df AS
+SELECT *
+FROM SP_Deployment
+WHERE environment = 'Production'
+  AND status = 'Decommissioned';
+
+-- Creating temporary table for deployments_filtered
+CREATE TEMP TABLE deployments_filtered AS
+SELECT d.*
+FROM deployments_df d
+LEFT JOIN exclude_classification_df e
+ON d.deploymentId = e.exclude_assetId
+WHERE e.exclude_assetId IS NULL;
+
+-- Creating temporary table for classification_bcp
+CREATE TEMP TABLE classification_bcp AS
+SELECT DISTINCT assetId, classificationId
+FROM classification_df
+WHERE classificationId IN (114, 115, 116, 117);
+
+-- Creating final temporary table dep_classification_bcp
+CREATE TEMP TABLE dep_classification_bcp AS
+SELECT d.*, c.classificationId,
+    CASE 
+        WHEN c.classificationId = 114 THEN 'tierA'
+        WHEN c.classificationId = 115 THEN 'tierBAsia'
+        WHEN c.classificationId = 116 AND d.rto IN (1,2) THEN 'tierBNonAsia'
+        WHEN c.classificationId = 117 AND d.rto IN (1,2) THEN 'tierC'
+        ELSE 'InvalidTier'
+    END AS checkBcpTier
+FROM deployments_filtered d
+LEFT JOIN classification_bcp c
+ON d.deploymentId = c.assetId;
+
